d3b6752df4e47b4e53e4bd31b6cf9fb644cc30cb
ananay <i@ananayarora.com>
2020-08-26 18:34:54 -0700

    Added /discord

---
+++ src/pages/discord.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function discord(props) {
+
+    window.location.href = 'https://discord.com/invite/uDnXrbh';
+
+    return (
+        <div>
+        </div>
+    )
+
+}
